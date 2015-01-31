4bed2f717607eadfe0a92312331ef4133d7d73d1
Tej Chajed <tchajed@gmail.com>
2015-01-31 18:16:53 -0500

    Initial basic recursive descent parser

---
+++ regexp.py
@@ -0,0 +1,137 @@
+__author__ = 'tchajed'
+
+
+class Regex:
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+
+class Choice(Regex):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.left) + "|" + str(self.right)
+
+    def __repr__(self):
+        return "Choice({}, {})".format(repr(self.left),
+                                       repr(self.right))
+
+
+class Empty(Regex):
+    pass
+
+    def __str__(self):
+        return ""
+
+    def __repr__(self):
+        return "Empty()"
+
+
+class Sequence(Regex):
+    def __init__(self, *elements):
+        self.elements = list(elements)
+
+    def add(self, el):
+        self.elements.append(el)
+
+    def simplify(self):
+        if len(self.elements) == 0:
+            return Empty()
+        if len(self.elements) == 1:
+            return self.elements[0]
+        return self
+
+    def __str__(self):
+        return "".join(map(str, self.elements))
+
+    def __repr__(self):
+        return "Sequence({})".format(
+            ", ".join(map(repr, self.elements))
+        )
+
+
+class Kleene(Regex):
+    def __init__(self, regex):
+        self.regex = regex
+
+    def __str__(self):
+        inner = str(self.regex)
+        if isinstance(self.regex, Char):
+            return inner + "*"
+        return "(" + inner + ")*"
+
+
+class Char(Regex):
+    def __init__(self, char):
+        self.char = char
+
+    def __str__(self):
+        return self.char
+
+    def __repr__(self):
+        inner = self.char
+        if inner in ["(", ")"]:
+            inner = "\\" + inner
+        return "Char({})".format(inner)
+
+
+class RegexParser:
+    def __init__(self, text):
+        self.text = text
+
+    def peek(self):
+        if len(self.text) == 0:
+            return None
+        return self.text[0]
+
+    def next(self):
+        n = self.text[0]
+        self.text = self.text[1:]
+        return n
+
+    def eat(self, item):
+        n = self.next()
+        if n != item:
+            raise ValueError("Expected {} but got {}".format(item, n))
+
+    # TODO: add groups
+
+    def regex(self):
+        term = self.term()
+        if self.peek() == '|':
+            self.eat("|")
+            regex = self.regex()
+            return Choice(term, regex)
+        return term
+
+    def term(self):
+        factors = Sequence()
+        while (self.peek() is not None and
+                self.peek() != ')' and self.peek() != '|'):
+            factor = self.factor()
+            factors.add(factor)
+        return factors.simplify()
+
+    def factor(self):
+        base = self.base()
+        if self.peek() == '*':
+            self.eat("*")
+            base = Kleene(base)
+        # TODO: support + repetition
+        return base
+
+    def base(self):
+        if self.peek() == "\\":
+            self.eat("\\")
+            return Char(self.next())
+        if self.peek() == "(":
+            self.eat("(")
+            regex = self.regex()
+            self.eat(")")
+            return regex
+        return Char(self.next())
---
+++ test/test_regexp.py
@@ -0,0 +1,49 @@
+__author__ = 'tchajed'
+
+import unittest
+from regexp import *
+
+
+class RegexpParserTestCase(unittest.TestCase):
+    def parse(self, s):
+        return RegexParser(s).regex()
+
+    def test_input_stream(self):
+        p = RegexParser("foo")
+        self.assertEquals(p.peek(), "f")
+        self.assertEquals(p.peek(), "f")
+        self.assertEquals(p.next(), "f")
+        self.assertEquals(p.peek(), "o")
+        self.assertEquals(p.next(), "o")
+        p.eat("o")
+        self.assertIsNone(p.peek())
+
+    def test_single_char(self):
+        r = self.parse("a")
+        self.assertEqual(r, Char("a"))
+
+    def test_choice(self):
+        r = self.parse("(ab)|c")
+        self.assertEqual(r, Choice(Sequence(Char("a"), Char("b")),
+                                   Char("c")))
+
+    def test_escape(self):
+        r = self.parse("""a\)c|\|""")
+        self.assertEqual(r, Choice(
+            Sequence(Char("a"),
+                     Char(")"),
+                     Char("c")),
+            Char("|")))
+
+    def test_kleene(self):
+        r = self.parse("a*(b|c)*df*")
+        self.assertEqual(r, Sequence(
+            Kleene(Char("a")),
+            Kleene(Choice(Char("b"), Char("c"))),
+            Char("d"),
+            Kleene(Char("f")),
+        ))
+
+
+if __name__ == '__main__':
+    unittest.main()
